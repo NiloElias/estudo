e4bb39aa523022120d5e98183928c8db26e856c4
LuizLins <luizmagao@gmail.com>
2021-09-14 22:13:29 -0300

    melhorando class banco

--- Conta.php
+++ Conta.php
@@ -5,6 +5,7 @@ class Conta
     private $agencia = '0001';
     private $conta;
     private $cliente;
+    private $valor;
 
     public function getAgencia()
     {
@@ -35,6 +36,24 @@ class Conta
     {
         $this->conta = $conta;
     }
+
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    private function setValor($valor)
+    {
+        $this->valor = $valor;
+    }
+
+    public function deposito($valor)
+    {
+        if ($valor > 0) {
+            $this->setValor($this->getValor() + $valor);
+        }
+        return;
+    }
 }
 
 $obj = new Conta;
@@ -43,10 +62,17 @@ $obj->setConta($_POST['conta']);
 $obj->setAgencia($_POST['agencia']);
 $obj->setCliente($_POST['cliente']);
 
+$obj->deposito((float) $_POST['valor']);
 
 $payload = [
     'error' => false,
-    'msg' => 'Conta aberta com sucesso!'
+    'msg' => 'Conta aberta com sucesso!',
+    'data' => [
+        'conta' => $obj->getConta(),
+        'agencia' => $obj->getAgencia(),
+        'cliente' => $obj->getCliente(),
+        'valor' => $obj->getValor(),
+    ]
 ];
 
 header('Content-Type: application/json; charset=utf-8');
